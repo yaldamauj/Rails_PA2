3701a3cd780033bbbbbf31c39c7f593578dbbfa2
yaldamauj <yaldamauj@brandeis.edu>
2021-03-04 19:27:07 -0500

    add a static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   resources :instructors
   resources :subjects
   resources :courses
-  root 'instructors#index'
+  root 'application#hello'
 end
