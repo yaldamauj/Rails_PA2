5275d4ba661ad588e60d03144422e1e8fe34258c
yaldamauj <yaldamauj@brandeis.edu>
2021-03-22 04:03:43 -0400

    added user model

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates(:name, presence: true, length: { maximum: 30 })
-    validates(:email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX })
-end
