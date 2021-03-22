7b1b0850ba934244218dcbecb9f5cdd367f4b373
yaldamauj <yaldamauj@brandeis.edu>
2021-03-22 14:41:04 -0400

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   get 'users/new'
   get 'static_pages/home'
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to:  "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "registrations#create"
+
   delete "logout", to: "sesions#destroy"
+  
   resources :instructors
   resources :subjects
   resources :courses
   resources :users
+
   root 'static_pages#home'
 end
