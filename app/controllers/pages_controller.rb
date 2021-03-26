87299130050e490776f8792fd5c47ab4a96e1e6c
yaldamauj <yaldamauj@brandeis.edu>
2021-03-25 22:45:28 -0400

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,6 @@ class PagesController < ApplicationController
         if params[:subject].blank?
             @courses = Course.where("name ILIKE ?", "%#{params[:search_text]}%")
           else
-            @courses = Subject.find(params[:subject]).courses.where("name ILIKE ?",
-                                    "%#{params[:search_text]}%")
           end
     end
 
